c17576d6f8b0715692ee865d2fe7bc925e9944d7
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2025-07-03 10:41:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,8 +257,8 @@ for estacao, (lat, lon) in coordenadas.items():
     df_completo.loc[df_completo['Nome'] == estacao, 'Longitude'] = lon
 
 st.subheader("Mapa Interativo das Estações Hidrológicas")
-#mapa = criar_mapa_completo(df_completo)
-#st_data = st_folium(mapa, width=1200, height=700, returned_objects=[])
+mapa = criar_mapa_completo(df_completo)
+st_data = st_folium(mapa, width=1200, height=700, returned_objects=[])
 df_completo
 df_estacoes
 df_graf
